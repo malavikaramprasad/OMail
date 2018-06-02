f789097487617a5f07dd88443a468aae3f2a42bd
Malavika Ramprasad <31526040+malavikaramprasad@users.noreply.github.com>
2018-06-01 20:31:44 -0500

    changes to keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
 	//Prod
-	module.exports = require('./prod.js');
+	module.exports = require('./prod');
 } else {
 	//Dev Env
-	module.exports = require('./dev.js');
+	module.exports = require('./dev');
 }
